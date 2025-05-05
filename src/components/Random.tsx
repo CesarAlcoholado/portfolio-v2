b3265e45211e5fbb08a8f2122ae51b6a24095fb1
Cesar <cesar.alcoholado@hotmail.com>
2025-05-05 19:49:52 -0300

    finishing tablet view

--- src/components/Random.tsx
+++ src/components/Random.tsx
@@ -1,6 +1,6 @@
 export const Random = () => {
   return (
-    <div className="w-full h-[130px] rounded-md bg-cardLight border-borderLight border-4 border-solid dark:bg-backgroundDark dark:border-borderDark shadow-sm shadow-gray-900/5 sm:grid-random sm:h-full sm:w-full sm:row-start-3 sm:row-end-4">
+    <div className="w-full h-[130px] rounded-md bg-cardLight border-borderLight border-4 border-solid dark:bg-backgroundDark dark:border-borderDark shadow-sm shadow-gray-900/5 sm:hidden">
       Random
     </div>
   );
