db18668ee2613c2b9a04cc20a53ef07dbaad1a3d
Cesar <cesar.alcoholado@hotmail.com>
2025-09-01 12:38:15 -0300

    fixed email card

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -6,7 +6,7 @@ export const Email = () => {
   const { theme } = usePortfolioContext()
 
   return (
-    <div className="w-full h-[80px] col-span-2 rounded-md bg-backgroundLight border-borderLight border-4 border-solid dark:bg-backgroundDark dark:border-borderDark shadow-sm shadow-gray-900/5 md:grid-email md:h-full md:row-start-3 md:row-end-4 md:col-start-2 md:col-end-3 xl:col-start-3 xl:col-end-4 xl:row-start-3 xl:row-end-4 card-animation scroll-reveal cursor-pointer">
+    <div className="w-full h-[80px] col-span-2 rounded-md bg-backgroundLight border-borderLight border-4 border-solid dark:bg-backgroundDark dark:border-borderDark shadow-sm shadow-gray-900/5 sm:h-[130px] md:grid-email md:h-full md:row-start-3 md:row-end-4 md:col-start-2 md:col-end-3 lg:col-start-3 lg:col-end-4 lg:row-start-3 lg:row-end-4 xl:col-start-3 xl:col-end-4 xl:row-start-3 xl:row-end-4 card-animation scroll-reveal cursor-pointer">
       <div className="h-full w-full border-gray-100 rounded-md border-[1px] flex flex-col justify-center items-center p-2 dark:border-gray-800">
         <span className="text-xs font-firaSans text-gray-500 dark:text-gray-200 md:text-base">
           CONTACT
--- src/components/ProjectsCard.tsx
+++ src/components/ProjectsCard.tsx
@@ -5,7 +5,7 @@ export const ProjectsCard = () => {
   const { theme } = usePortfolioContext()
 
   return (
-    <div className="relative w-full h-[130px] rounded-md border-blue-600/10 border-4 border-solid shadow-sm shadow-gray-900/5 sm:grid-projects sm:row-start-6 sm:row-end-8 lg:h-full lg:col-start-3 lg:col-end-4 lg:row-start-2 lg:row-end-4 xl:col-start-3 xl:col-end-4 xl:row-start-2 xl:row-end-3 overflow-hidden card-animation scroll-reveal dark:bg-[linear-gradient(to_right,_rgba(79,70,229,0.1)_0%,_rgba(8,145,178,0.1)_100%)] hover:border-blue-600/40 cursor-pointer">
+    <div className="relative w-full h-[130px] rounded-md border-blue-600/10 border-4 border-solid shadow-sm shadow-gray-900/5 sm:grid-projects sm:row-start-6 sm:row-end-8 lg:h-full lg:col-start-3 lg:col-end-4 lg:row-start-2 lg:row-end-3 xl:col-start-3 xl:col-end-4 xl:row-start-2 xl:row-end-3 overflow-hidden card-animation scroll-reveal dark:bg-[linear-gradient(to_right,_rgba(79,70,229,0.1)_0%,_rgba(8,145,178,0.1)_100%)] hover:border-blue-600/40 cursor-pointer">
       <div className="h-full w-full border-gray-100 rounded-md border-[1px] flex flex-col justify-center items-center gap-1.5 p-2 dark:border-gray-800">
         <div className="flex items-center gap-2">
           <span className="text-xs font-firaSans text-gray-500 dark:text-gray-200 md:text-base">
